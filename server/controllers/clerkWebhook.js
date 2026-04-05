2b3ba088480719de0743aa1e792030a19010df79
thinh <phamlethinh920@gamil.com>
2026-04-05 23:35:23 +0700

    backend

--- server/controllers/clerkWebhook.js
+++ server/controllers/clerkWebhook.js
@@ -50,7 +50,7 @@ const clerkWebhooks = async(req, res)=>{
     }
     catch(error){
       console.log(error.message)
-      res.json({success:false, message:error.message})
+      res.json({succes:false, message:error.message})
 
     }
   
